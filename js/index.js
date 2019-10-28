c2961e16b54d6a91b277d8b97f83bff982a20e2c
David Tauraso <david318@me.com>
2019-10-28 14:21:57 -0700

    setup remaining data

--- js/index.js
+++ js/index.js
@@ -74,16 +74,89 @@ console.log(call_to_action.children)
 //   "button": "Get Started",
 //   "img-src": "img/header-img.png"
 // }
-call_to_action.querySelector(".cta-text").children[0].setAttribute("innerHTML", siteContent["cta"]["h1"])
-call_to_action.querySelector(".cta-text").children[1].setAttribute("innerHTML", siteContent["cta"]["button"])
+call_to_action.querySelector(".cta-text").children[0].innerHTML = siteContent["cta"]["h1"]
+call_to_action.querySelector(".cta-text").children[1].innerHTML = siteContent["cta"]["button"]
 call_to_action.children[1].setAttribute("src", siteContent["cta"]["img-src"])
 
 let main_content = document.querySelector(".main-content")
-// console.log(main_content)
+console.log(main_content)
+// "main-content": {
+//   "features-h4":"Features",
+//   "features-content": "Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+//   "about-h4":"About",
+//   "about-content": "About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+//   "middle-img-src": "img/mid-page-accent.jpg",
+//   "services-h4":"Services",
+//   "services-content": "Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+//   "product-h4":"Product",
+//   "product-content": "Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+//   "vision-h4":"Vision",
+//   "vision-content": "Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+// }
+
+let top_content = main_content.querySelector(".top-content")
+let top_content_text_content_1 = top_content.children[0]
+let top_content_text_content_2 = top_content.children[1]
+
+let image = main_content.querySelector("img")
+let bottom_content = main_content.querySelector(".bottom-content")
+let bottom_content_text_content_1 = bottom_content.children[0]
+let bottom_content_text_content_2 = bottom_content.children[1]
+let bottom_content_text_content_3 = bottom_content.children[2]
+
+console.log(top_content)
+console.log(top_content_text_content_1)
+console.log(top_content_text_content_1.children)
+// when filling the text between tags have to use
+// the .innerHTML property directly
+top_content_text_content_1.children[0].innerHTML = siteContent["main-content"]["features-h4"]
+top_content_text_content_1.children[1].innerHTML = siteContent["main-content"]["features-content"]
+
+
+// console.log(top_content_text_content_2)
+top_content_text_content_2.children[0].innerHTML = siteContent["main-content"]["about-h4"]
+top_content_text_content_2.children[1].innerHTML = siteContent["main-content"]["about-content"]
+
+// console.log(image)
+image.setAttribute("src", siteContent["main-content"]["middle-img-src"])
+// console.log(bottom_content)
+
+// console.log(bottom_content_text_content_1)
+bottom_content_text_content_1.children[0].innerHTML = siteContent["main-content"]["services-h4"]
+bottom_content_text_content_1.children[1].innerHTML = siteContent["main-content"]["services-content"]
+
+bottom_content_text_content_2.children[0].innerHTML = siteContent["main-content"]["product-h4"]
+bottom_content_text_content_2.children[1].innerHTML = siteContent["main-content"]["product-content"]
+
+bottom_content_text_content_3.children[0].innerHTML = siteContent["main-content"]["vision-h4"]
+bottom_content_text_content_3.children[1].innerHTML = siteContent["main-content"]["vision-content"]
+// console.log(bottom_content_text_content_2)
+// bottom_content_text_content_2.innerHTML = siteContent["main-content"]["services-content"]
+// console.log(bottom_content_text_content_3)
+// console.log(bottom_content_text_content_3)
+// bottom_content_text_content_3.innerHTML = siteContent["main-content"]["services-content"]
 
 let contact = document.querySelector(".contact")
-// console.log(contact)
+console.log(contact)
+// "contact": {
+//   "contact-h4" : "Contact",
+//   "address" : "123 Way 456 Street Somewhere, USA",
+//   "phone" : "1 (888) 888-8888",
+//   "email" : "sales@greatidea.io",
+// }
+contact.querySelector("h4").innerHTML = siteContent["contact"]["contact-h4"]
+contact.children[1].innerHTML = siteContent["contact"]["address"]
+
+contact.children[2].innerHTML = siteContent["contact"]["phone"]
+
+contact.children[3].innerHTML = siteContent["contact"]["email"]
 
 let footer = document.querySelector("footer")
 
 // console.log(footer)
+
+// "footer": {
+//   "copyright" : "Copyright Great Idea! 2018"
+// }
+
+footer.querySelector("p").innerHTML = siteContent["footer"]["copyright"]
